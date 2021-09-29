af3e56629989919e9758edfe2c169636eaa79b95
cedricchristoph <cedric@icecraft.es>
2021-09-29 21:52:51 +0100

    Terminado ejercicio de numeros primos

---
+++ TRIM1/algoritmoPrimos/src/algoritmoprimos/Algoritmo.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmoprimos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Cedric Christoph
+ */
+public class Algoritmo {
+    
+    final static int PROGRESS_CHECK = 5000;
+    static int antiguo;
+    int limit;
+    
+    /**
+     * Constructor de la clase Algoritmo
+     * @param limit Limite a encontrar numeros primos
+     */
+    public Algoritmo(int limit) {
+        this.limit = limit;
+    }
+    
+    /**
+     * Metodo para iniciar el algoritmo
+     */
+    public void iniciarAlgoritmo() {
+        ArrayList<Integer> resultadosPrimos = new ArrayList<>();
+        int counter = 0;
+        antiguo = 2;
+        for (int i = 2; i < limit; i++) {
+            if ((i - antiguo) == PROGRESS_CHECK) {
+                antiguo = i;
+                double porcentaje = ((i / (double) limit) * 100);
+                System.out.println("Procesando... ( " + i + " / " + limit + " ) " + String.format("%.2f", porcentaje) + "%");
+            }
+            if (esPrimo(i))
+                resultadosPrimos.add(i);
+        }
+        System.out.println("\nAlgoritmo completado ( " + limit + " / " + limit + " ) 100.00%\n\nResultados:");
+        System.out.println("Existen " + resultadosPrimos.size() + " numeros primos en los primeros " + limit + " numeros");
+    }
+    
+    /**
+     * Funcion que determina si el numero indicado es o no primo
+     * @param numero Numero a comprobar
+     * @return devuelve true y solo true si el numero es primo
+     */
+    private boolean esPrimo(int numero) {
+        for (int i = 2; i < numero; i++) {
+            if (numero % i == 0) 
+                return false;
+        }
+        return true;
+    }
+}
--- TRIM1/algoritmoPrimos/src/algoritmoprimos/AlgoritmoPrimos.java
+++ TRIM1/algoritmoPrimos/src/algoritmoprimos/AlgoritmoPrimos.java
@@ -8,6 +8,7 @@ package algoritmoprimos;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Formatter;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -16,7 +17,7 @@ import java.util.Scanner;
  */
 public class AlgoritmoPrimos {
 
-    final static int PROGRESS_CHECK = 10;
+    final static int PROGRESS_CHECK = 1000;
     static int actual;
     static int antiguo;
     
@@ -24,38 +25,25 @@ public class AlgoritmoPrimos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Introducir limite a comprobar numeros primos:");
-        Integer limit = scanner.nextInt();
-        System.out.println("");
-        iniciarAlgoritmo(limit);
-        
-    }
-    
-    private static void iniciarAlgoritmo(Integer limit) {
-        ArrayList<Integer> resultadosPrimos = new ArrayList<>();
-        int counter = 0;
-        antiguo = 2;
-        for (int i = 2; i < limit; i++) {
-            if ((i - antiguo) == PROGRESS_CHECK) {
-                antiguo = i;
-                double porcentaje = ((i / (double) limit) * 100);
-                System.out.println("Procesando... ( " + i + " / " + limit + " ) " + porcentaje + "%");
+        Integer limit;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Introducir limite a comprobar numeros primos: (Introduce 0 para terminar el programa)");
+            try {
+                limit = scanner.nextInt();
+                System.out.println("");
+                if (!(limit == 0)) {
+                    Algoritmo algoritmo = new Algoritmo(limit);
+                    algoritmo.iniciarAlgoritmo();
+                } else {
+                    System.exit(0);
+                }
+            } catch (InputMismatchException e) {
+                
             }
-            if (esPrimo(i))
-                resultadosPrimos.add(i);
         }
-        System.out.println("Existen " + resultadosPrimos.size() + " numeros primos en los primeros " + limit + " numeros");
     }
     
-    private static boolean esPrimo(int numero) {
-        for (int i = 2; i < numero; i++) {
-            if (numero % i == 0) 
-                return false;
-        }
-        return true;
-    }
     
     
 }
